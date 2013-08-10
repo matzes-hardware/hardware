1ac77b98138fe94a99b901fedad8b17b8205148c
Matthias <mail@matthiasbock.net>
2013-08-10 18:42:49 +0200

    +Trimble Lassen SK-II

---
+++ Trimble Lassen SK-II/Trimble - Lassen SK II_files/sli-rac.config.js
@@ -0,0 +1,7 @@
+(function(window){
+/*!*/
+var racOpts={version:"1.5",base:"http://trimble.resultspage.com",onsubmit:undefined,align:"right"};
+/*!*/
+if(document.location.protocol=="https:"){racOpts.base=racOpts.base.replace(/^https?:/i,"https:")}if(document.location.href.match(/\.local|resultsdemo/)){racOpts.base="http://"+document.domain}racOpts.onsubmit=function(param){};Function.prototype.slibind=function(obj){var method=this,temp=function(){return method.apply(obj,arguments)};return temp};var sliAutocomplete={opts:{version:"",path:"http://assets.resultspage.com/js/rac/sli-rac.stub",ext:"js",https:true},init:function(opts){for(var i in opts){this.opts[i]=opts[i]
+}if(window.sli&&window.sli.global&&window.sli.global.path){this.opts.path=window.sli.global.path}this.load()},load:function(){var obj=this;if(obj.oScript){obj.stubInit()}else{obj.oScript=document.createElement("script");obj.oScript.type="text/javascript";var path=obj.opts.path+".";if(obj.opts.version!=""){path+=obj.opts.version+"."}path+=obj.opts.ext;if(obj.opts.https&&document.location.protocol=="https:"){path=path.replace(/^https?:/i,"https:")}obj.oScript.src=path;document.body.appendChild(obj.oScript)}},extend:function(obj){if(obj.extend){obj.extend(this)
+}else{for(var i in obj){this[i]=obj[i]}}}};window.sliAutocomplete=sliAutocomplete;if(jQuery.ready){jQuery(document).ready(function(){window.sliAutocomplete.init(racOpts)})}else{window.sliAutocomplete.init(racOpts)}})(window);
